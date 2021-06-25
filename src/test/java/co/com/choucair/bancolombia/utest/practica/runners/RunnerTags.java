3e5dbe55dc9ab9b309491c4d7e78cd876914968e
Hroyero <37231471+Hroyero@users.noreply.github.com>
2021-06-24 19:07:06 -0500

    se agrego snippets
    se agrego SnippteType.CAMELCASE

--- src/test/java/co/com/choucair/bancolombia/utest/practica/runners/RunnerTags.java
+++ src/test/java/co/com/choucair/bancolombia/utest/practica/runners/RunnerTags.java
@@ -1,6 +1,7 @@
 package co.com.choucair.bancolombia.utest.practica.runners;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
@@ -8,8 +9,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features/register_utest.feature",
         tags = "@stories",
-        glue = "co.com.choucair.bancolombia.utest.practica.stepdefinitions"
-)
+        glue = "co.com.choucair.bancolombia.utest.practica.stepdefinitions",
+        snippets = SnippetType.CAMELCASE)
 public class RunnerTags {
 
 }
